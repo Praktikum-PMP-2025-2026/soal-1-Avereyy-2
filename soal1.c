092498133ee8a10ac5f715a724f951230349919d
Michael Reynaldi Tamara <117028090+Avereyy@users.noreply.github.com>
2026-05-06 07:50:50 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -7,26 +7,29 @@ int main() {
     char id[N][10];
     int durasi[N];
 
+    // input
     for (int i = 0; i < N; i++) {
         scanf("%s %d", id[i], &durasi[i]);
     }
 
-    // ORDER
-    printf("ORDER");
+    // output ORDER
+    printf("ORDER ");
     for (int i = 0; i < N; i++) {
-        printf(" %s", id[i]);
+        printf("%s", id[i]);
+        if (i != N - 1) printf(" ");
     }
     printf("\n");
 
+    // hitung WAIT
     int total_wait = 0;
     int current_time = 0;
 
-    for (int i = 0; i < N - 1; i++) { // ❗ stop at N-1
+    for (int i = 0; i < N; i++) {
         total_wait += current_time;
         current_time += durasi[i];
     }
 
-    printf("WAIT %d", total_wait);
+    printf("WAIT %d\n", total_wait);
 
     return 0;
 }
