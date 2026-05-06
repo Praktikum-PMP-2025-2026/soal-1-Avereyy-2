5130a7aa264be2aff64345964778d7d2b7278fe2
Michael Reynaldi Tamara <117028090+Avereyy@users.noreply.github.com>
2026-05-06 07:44:39 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    char id[N][10];
+    int durasi[N];
+
+    // input
+    for (int i = 0; i < N; i++) {
+        scanf("%s %d", id[i], &durasi[i]);
+    }
+
+    // output ORDER
+    printf("ORDER ");
+    for (int i = 0; i < N; i++) {
+        printf("%s", id[i]);
+        if (i != N - 1) printf(" ");
+    }
+    printf("\n");
+
+    // hitung WAIT
+    int total_wait = 0;
+    int current_time = 0;
+
+    for (int i = 0; i < N; i++) {
+        total_wait += current_time;
+        current_time += durasi[i];
+    }
+
+    printf("WAIT %d\n", total_wait);
+
     return 0;
 }
