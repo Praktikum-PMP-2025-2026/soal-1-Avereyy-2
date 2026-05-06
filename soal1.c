22f57365097029f770e25657b463df550e663ad9
Michael Reynaldi Tamara <117028090+Avereyy@users.noreply.github.com>
2026-05-06 07:49:15 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -2,32 +2,30 @@
 
 int main() {
     int N;
-    scanf("%d", &N);
 
-    char id[N][10];
-    int durasi[N];
+    if (scanf("%d", &N) != 1) return 0;
 
+    int arr[N];
     for (int i = 0; i < N; i++) {
-        scanf("%s %d", id[i], &durasi[i]);
+        if (scanf("%d", &arr[i]) != 1) return 0;
     }
 
-    // ORDER
-    printf("ORDER");
-    for (int i = 0; i < N; i++) {
-        printf(" %s", id[i]);
+    int level = 0;
+    int index = 0;
+
+    while (index < N) {
+        int nodes = 1 << level;  // 2^level TANPA pow()
+
+        printf("LEVEL %d:", level);
+
+        for (int i = 0; i < nodes && index < N; i++) {
+            printf(" %d", arr[index]);
+            index++;
+        }
+
+        if (index < N) printf("\n");  // ❗ no extra newline at end
+        level++;
     }
-    printf("\n");
-
-    // WAIT
-    int total_wait = 0;
-    int current_time = 0;
-
-    for (int i = 0; i < N; i++) {
-        total_wait += current_time;
-        current_time += durasi[i];
-    }
-
-    printf("WAIT %d", total_wait); // ❗ no extra newline at end
 
     return 0;
 }
